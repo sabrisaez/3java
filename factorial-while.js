e57e1162a298cab76240cf416c700e9aa2a506e3
Sabrina <sabrinasaeztoro@gmail.com>
2023-07-23 05:28:18 -0300

    factorial while

---
+++ factorial-while.js
@@ -0,0 +1,9 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let factorial = 1;
+let num = 10;
+while (num > 1) {
+  factorial *= num;
+  num--;
+}
+console.log(factorial);
